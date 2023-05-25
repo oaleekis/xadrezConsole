b6bfaaf1a5f0b72ca2183e583c16d1f3b05c31d2
Alex <oalexsanderm@gmail.com>
2023-05-25 11:06:43 -0300

    Adicionar arquivos de projeto.

---
+++ xadrezConsole/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using tabuleiro;
+
+namespace xadrezConsole
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Posicao p;
+
+            p = new Posicao(3, 4);
+            Console.WriteLine("Posiçao: " + p);
+
+        }
+    }
+}
